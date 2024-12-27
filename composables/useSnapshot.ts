bed44229ddcfee25786e298f1f6442a4fe2e1cf7
Ariz <lunari8546@gmail.com>
2024-12-27 16:13:20 +1100

    fix: consistent snapshot dimensions

--- composables/useSnapshot.ts
+++ composables/useSnapshot.ts
@@ -5,12 +5,16 @@ export function useSnapshot() {
 
   const { currentSlides } = storeToRefs(useDeckStore());
 
-  const capture = async (deck: string) => {
-    if (!currentSlides.value || useRoute().params.id !== deck) return;
+  const capture = async () => {
+    if (!currentSlides.value) return;
 
     const blob = await html2canvas(document.querySelector(".render")!, {
-      scale: 2,
+      scale: 1,
       useCORS: true,
+      onclone: (document, element) => {
+        element.style.width = "1920px";
+        element.style.height = "1080px";
+      },
     }).then(
       (canvas) =>
         new Promise<Blob>((resolve) =>
@@ -20,10 +24,14 @@ export function useSnapshot() {
 
     const { error } = await client.storage
       .from("snapshots")
-      .upload(`${deck}/${currentSlides.value.id}.png`, blob, {
-        upsert: true,
-        contentType: "image/png",
-      });
+      .upload(
+        `${currentSlides.value.deck}/${currentSlides.value.id}.png`,
+        blob,
+        {
+          upsert: true,
+          contentType: "image/png",
+        }
+      );
 
     if (error) throw error;
   };
