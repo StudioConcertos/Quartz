977fa6899f1e9e3697e2882a84a1020b5788aa67
Ariz <lunari8546@gmail.com>
2025-01-07 21:43:46 +1100

    wip: updated temporal hero

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,4 @@
   <Title>Quartz</Title>
   <LandingNavbar />
   <LandingHero />
-  <section class="flex flex-col justify-center items-center">
-    <p>Landing page is still work in progress.</p>
-  </section>
 </template>
