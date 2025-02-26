335d83fbd5c43ea524070a62b684a65e05f5ebb0
Ariz <lunari8546@gmail.com>
2025-02-26 15:34:34 +1100

    wip: assets upload

--- components/atelier/inspector/view/Assets.vue
+++ components/atelier/inspector/view/Assets.vue
@@ -1,5 +1,40 @@
 <template>
-  <AtelierInspectorView name="Assets" :actions="[]">
+  <AtelierInspectorView
+    name="Assets"
+    :actions="[
+      {
+        icon: 'i-carbon-cloud-upload',
+        tooltip: 'Upload asset',
+        onClick: () => open(),
+      },
+    ]"
+  >
     <p>test</p>
   </AtelierInspectorView>
 </template>
+
+<style scoped lang="postcss"></style>
+
+<script setup lang="ts">
+const client = useSupabaseClient<Database>();
+
+const { currentSlides } = storeToRefs(useDeckStore());
+
+const { files, open, onChange } = useFileDialog({
+  accept: "image/*",
+});
+
+onChange(async (files) => {
+  if (!files?.length) return;
+
+  for (const file of files) {
+    const { error } = await client.storage
+      .from("assets")
+      .upload(`${currentSlides.value.deck}/${file.name}`, file);
+
+    if (error) {
+      console.error(error);
+    }
+  }
+});
+</script>
