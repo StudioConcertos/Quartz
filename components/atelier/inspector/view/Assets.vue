a4652427968a8570367dc03abed643c3f9e241af
Ariz <lunari8546@gmail.com>
2025-03-04 19:39:22 +1100

    wip: image preview

--- components/atelier/inspector/view/Assets.vue
+++ components/atelier/inspector/view/Assets.vue
@@ -9,33 +9,43 @@
       },
     ]"
   >
-    <div v-if="assets.length" class="grid grid-cols-4 gap-4">
+    <div v-if="assets.length" class="list">
       <div v-for="asset in assets">
-        <div
-          v-if="
-            asset.name.endsWith('.png') ||
-            asset.name.endsWith('.jpg') ||
-            asset.name.endsWith('.jpeg')
-          "
-          class="relative aspect-square overflow-hidden rounded-lg"
-        >
-          <NuxtImg
-            :src="asset.url.toString()"
-            :alt="asset.name"
-            class="w-full h-full object-cover"
-          />
-        </div>
+        <button v-if="isImage(asset.name)" @click="openModal(asset)">
+          <NuxtImg :src="asset.url.toString()" :alt="asset.name" />
+        </button>
         <div v-else>
-          <p>Unspported asset - {{ asset.name }}</p>
+          <p>Unsupported asset - {{ asset.name }}</p>
         </div>
       </div>
     </div>
+    <Modal ref="modal" :title="`${previewImage?.name}`">
+      <NuxtImg
+        @click="modal?.close()"
+        :src="previewImage?.url.toString()"
+        alt="preview"
+      />
+    </Modal>
   </AtelierInspectorView>
 </template>
 
-<style scoped lang="postcss"></style>
+<style scoped lang="postcss">
+.list {
+  @apply grid grid-cols-4 gap-4;
+
+  button {
+    @apply relative aspect-square overflow-hidden rounded-lg;
+
+    img {
+      @apply w-full h-full object-cover;
+    }
+  }
+}
+</style>
 
 <script setup lang="ts">
+import type Modal from "@/components/Modal.vue";
+
 const client = useSupabaseClient<Database>();
 
 const { currentSlides } = storeToRefs(useDeckStore());
@@ -58,4 +68,19 @@ onChange(async (files) => {
     }
   }
 });
+
+const isImage = (asset: string) => {
+  return (
+    asset.endsWith(".png") || asset.endsWith(".jpg") || asset.endsWith(".jpeg")
+  );
+};
+
+const previewImage = ref<{ name: string; url: URL }>();
+const modal = ref<typeof Modal>();
+
+function openModal(asset: { name: string; url: URL }) {
+  modal.value?.open();
+
+  previewImage.value = asset;
+}
 </script>
