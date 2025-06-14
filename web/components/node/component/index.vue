a581588fd1156eb073df00660d28462e273ff154
Ariz <lunari8546@gmail.com>
2025-06-14 16:26:17 +1000

    wip: improved component UI

--- web/components/node/component/index.vue
+++ web/components/node/component/index.vue
@@ -1,45 +1,62 @@
 <template>
-  <div class="component">
-    <div class="flex items-center justify-between">
-      <h4>{{ props.name.toUpperCase() }}</h4>
-      <button @click="isOpen = !isOpen">
-        <div
-          :class="isOpen ? 'i-carbon-caret-down' : 'i-carbon-caret-right'"
-        ></div>
-      </button>
+  <div :class="{ 'opacity-100!': isOpen }" class="component" tabindex="0">
+    <header @click="toggleComponent">
+      <h4>
+        <div class="icon" :class="icon"></div>
+        {{ props.name.toUpperCase() }}
+      </h4>
+      <div
+        :class="isOpen ? 'i-carbon-caret-down' : 'i-carbon-caret-right'"
+        class="arrow"
+      ></div>
+    </header>
+    <div v-if="isOpen" class="fields">
+      <slot />
     </div>
-    <div v-if="isOpen" class="whitespace"></div>
-    <slot v-if="isOpen" />
   </div>
 </template>
 
 <style scoped lang="postcss">
 .component {
-  @apply w-full p-6 bg-dark-900;
+  @apply w-full bg-dark-900;
   @apply border-solid border-0 border-b-2 border-dark-200;
+  @apply opacity-60 hover:opacity-100;
+  @apply transition-opacity duration-150;
 
-  h4 {
-    @apply text-sm font-400 opacity-60;
-  }
+  header {
+    @apply flex items-center justify-between p-6 cursor-pointer;
 
-  button {
-    @apply text-2xl;
-    @apply opacity-60 transition-opacity hover:opacity-100;
-  }
+    h4 {
+      @apply font-300 opacity-80;
+      @apply flex items-center gap-2;
 
-  &:deep(.field) {
-    @apply flex;
-
-    &:not(:last-child) {
-      @apply mb-6;
+      .icon {
+        @apply text-xl mt-0.5;
+      }
     }
 
-    label {
-      @apply text-sm w-1/3;
+    .arrow {
+      @apply text-2xl opacity-80;
     }
+  }
 
-    > *:not(label) {
-      @apply p-0 flex-1;
+  .fields {
+    @apply px-6 pb-6;
+
+    &:deep(.field) {
+      @apply flex;
+
+      &:not(:last-child) {
+        @apply mb-6;
+      }
+
+      label {
+        @apply text-sm w-1/3;
+      }
+
+      > *:not(label) {
+        @apply p-0 flex-1;
+      }
     }
   }
 }
@@ -50,5 +67,34 @@ const props = defineProps<{
   name: string;
 }>();
 
-const isOpen = ref(true);
+const isOpen = ref(false);
+
+const icon = computed(() => {
+  switch (props.name) {
+    case "base":
+      return "i-carbon-array";
+
+    case "camera":
+      return "i-carbon-camera";
+
+    case "layout":
+      return "i-carbon-template";
+
+    case "model":
+      return "i-carbon-model-alt";
+
+    case "scene":
+      return "i-carbon-web-services-container";
+
+    case "transform":
+      return "i-carbon-shapes";
+
+    case "typography":
+      return "i-carbon-text-font";
+  }
+});
+
+function toggleComponent() {
+  isOpen.value = !isOpen.value;
+}
 </script>
